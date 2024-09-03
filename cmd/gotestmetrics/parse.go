9c7df1a3e9f60223c5b21f45416d6cab19783549
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-09-03 15:43:15 +0200

    debug go test output
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- cmd/gotestmetrics/parse.go
+++ cmd/gotestmetrics/parse.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,34 +17,67 @@ type parseCmd struct {
 }
 
 func (c *parseCmd) Run(ctx *Context) error {
-	res, ex, err := gotest.Parse(gotest.ParseConfig{
-		Stdout: os.Stdin,
-		Logger: log.Writer(),
-	})
-	if err != nil {
-		return errors.Wrapf(err, "failed to scan test output")
-	}
+	pr, pw := io.Pipe()
+	var buf bytes.Buffer
+	mw := io.MultiWriter(&buf, pw)
 
-	if ctx.Debug {
-		b, _ := json.MarshalIndent(res, "", "  ")
-		log.Printf("%s", string(b))
-	}
+	errCh := make(chan error, 1)
 
-	if len(ex.Failed()) > 0 {
-		return errors.Errorf("%d test(s) failed", len(ex.Failed()))
-	}
-
-	if c.Output != "" {
-		if err := os.MkdirAll(filepath.Dir(c.Output), 0755); err != nil {
-			return errors.Wrap(err, "failed to create output file directory")
-		}
-		dt, err := json.MarshalIndent(res, "", "  ")
+	go func() {
+		defer pr.Close()
+		res, ex, err := gotest.Parse(gotest.ParseConfig{
+			Stdout: pr,
+			Logger: log.Writer(),
+		})
 		if err != nil {
-			return errors.Wrap(err, "failed to marshal result")
+			errCh <- errors.Wrap(err, "failed to scan test output")
+			return
 		}
-		if err := os.WriteFile(c.Output, dt, 0644); err != nil {
-			return errors.Wrap(err, "failed to write result to output file")
+
+		if ctx.Debug {
+			b, _ := json.MarshalIndent(res, "", "  ")
+			log.Printf("%s", string(b))
 		}
+
+		if len(ex.Failed()) > 0 {
+			errCh <- errors.Errorf("%d test(s) failed", len(ex.Failed()))
+			return
+		}
+
+		if c.Output != "" {
+			if err := os.MkdirAll(filepath.Dir(c.Output), 0755); err != nil {
+				errCh <- errors.Wrap(err, "failed to create output file directory")
+				return
+			}
+			dt, err := json.MarshalIndent(res, "", "  ")
+			if err != nil {
+				errCh <- errors.Wrap(err, "failed to marshal result")
+				return
+			}
+			if err := os.WriteFile(c.Output, dt, 0644); err != nil {
+				errCh <- errors.Wrap(err, "failed to write result to output file")
+				return
+			}
+		}
+
+		errCh <- nil
+	}()
+
+	_, err := io.Copy(mw, os.Stdin)
+	if err != nil {
+		return errors.Wrap(err, "failed to copy test output")
 	}
+
+	pw.Close()
+
+	_, err = io.Copy(os.Stdout, &buf)
+	if err != nil {
+		return errors.Wrap(err, "failed to write buffer to stdout")
+	}
+
+	if err := <-errCh; err != nil {
+		return err
+	}
+
 	return nil
 }
