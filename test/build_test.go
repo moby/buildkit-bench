155d93c11dee14c4dd647682ef8ece7e5e918f64
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-09-12 16:47:39 +0200

    test: build breaker
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- test/build_test.go
+++ test/build_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -14,6 +15,10 @@ func BenchmarkBuild(b *testing.B) {
 		benchmarkBuildLocal,
 		benchmarkBuildLocalSecret,
 		benchmarkBuildRemoteBuildme,
+		benchmarkBuildBreaker16,
+		benchmarkBuildBreaker32,
+		benchmarkBuildBreaker64,
+		benchmarkBuildBreaker128,
 	), testutil.WithMirroredImages(testutil.OfficialImages(
 		"busybox:latest",
 		"golang:1.22-alpine",
@@ -73,4 +78,71 @@ func benchmarkBuildRemoteBuildme(b *testing.B, sb testutil.Sandbox) {
 	))
 	testutil.ReportMetricDuration(b, time.Since(start))
 	require.NoError(b, err, out)
+}
+
+func benchmarkBuildBreaker16(b *testing.B, sb testutil.Sandbox) {
+	buildBreaker(b, sb, 16)
+}
+
+func benchmarkBuildBreaker32(b *testing.B, sb testutil.Sandbox) {
+	buildBreaker(b, sb, 32)
+}
+
+func benchmarkBuildBreaker64(b *testing.B, sb testutil.Sandbox) {
+	buildBreaker(b, sb, 64)
+}
+
+func benchmarkBuildBreaker128(b *testing.B, sb testutil.Sandbox) {
+	buildBreaker(b, sb, 128)
+}
+
+func buildBreaker(b *testing.B, sb testutil.Sandbox, n int) {
+	type buildRecord struct {
+		id  int
+		d   time.Duration
+		out string
+		err error
+	}
+
+	var wg sync.WaitGroup
+	records := make(chan buildRecord, 5)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			start := time.Now()
+			out, err := buildCmd(sb, withArgs(
+				"--opt=context=https://github.com/dvdksn/buildme.git#eb6279e0ad8a10003718656c6867539bd9426ad8",
+				"--opt=build-arg:BUILDKIT_SYNTAX=docker/dockerfile:1.9.0", // pin dockerfile syntax
+			))
+			d := time.Since(start)
+			b.Logf("build %d: %fs", i, d.Seconds())
+			record := buildRecord{
+				id:  i,
+				d:   d,
+				out: out,
+				err: err,
+			}
+			records <- record
+			require.NoError(b, err, out)
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(records)
+	}()
+
+	var avgd time.Duration
+	var totald time.Duration
+	for record := range records {
+		if record.err != nil {
+			b.Fatalf("build %d failed: %v", record.id, record.err)
+		}
+		totald += record.d
+	}
+	avgd = totald / time.Duration(n)
+	testutil.ReportMetricDuration(b, avgd)
 }
