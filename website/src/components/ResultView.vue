a42e24bb76eb054085a90127f661175356750be1
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-09-15 22:27:41 +0200

    website: use BASE_URL env
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- website/src/components/ResultView.vue
+++ website/src/components/ResultView.vue
@@ -5,15 +5,12 @@
 </template>
 
 <script>
-import { publicPath } from '../../vue.config';
 
 export default {
   name: 'ResultView',
   computed: {
     resultUrl() {
-      const resultLink = `${publicPath}result/${this.$route.params.result}/index.html`;
-      console.log(`Loading result ${resultLink}`);
-      return resultLink;
+      return `${process.env.BASE_URL}result/${this.$route.params.result}/index.html`;
     }
   }
 };
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ResultView from '@/components/ResultView.vue';
-import { publicPath } from '../../vue.config';
 
 Vue.use(Router);
 
 export default new Router({
-  base: publicPath,
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/result/:result',
